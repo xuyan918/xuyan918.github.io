55207fd52be3e5914108a515d18471885489a487
xy <xy@192.168.5.160>
2026-07-31 17:30:15 +0800

    Merge jobs into growth and refresh bear artwork

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = "bear-workbench-v23";
-const ASSETS = ["/", "/manifest.webmanifest?v=2", "/icons/icon-192.png?v=2", "/icons/icon-512.png?v=2", "/icons/apple-touch-icon.png?v=2", "/bears/app-bear.jpg", "/bears/bear-grid.jpg", "/bears/bear-ribbon.jpg", "/bears/bear-trio.jpg"];
+const CACHE = "bear-workbench-v24";
+const ASSETS = ["/", "/manifest.webmanifest?v=3", "/icons/icon-192.png?v=3", "/icons/icon-512.png?v=3", "/icons/apple-touch-icon.png?v=3", "/bears/v2/bear-heart.png", "/bears/v2/bear-reading.png", "/bears/v2/bear-work.png", "/bears/v2/bear-ribbon.png", "/bears/v2/bear-flower.png", "/bears/v2/bear-sleep.png", "/bears/v2/bear-travel.png", "/bears/v2/bear-fitness.png", "/bears/v2/bear-profile.png"];
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener("fetch", (event) => {
